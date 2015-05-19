35e460421c07fda386e32c2c08eeaf5b91d6bce4
HellFireAE <hellfireae@gmail.com>
2015-05-19 13:01:19 -0400

    Added existing check. Fails nicely now

--- js/bootstrap-carousel-single-push.js
+++ js/bootstrap-carousel-single-push.js
@@ -1,14 +1,18 @@
-$('.carousel.single-push .item').each(function() {
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
+jQuery(document).ready(function($) {
+  if ($('.carousel.single-push .item').length > 0) {
+    $('.carousel.single-push .item').each(function() {
+      var next = $(this).next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
 
-  next = next.next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
+      next = next.next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
 
-  next.children(':first-child').clone().appendTo($(this));
+      next.children(':first-child').clone().appendTo($(this));
+    });
+  }
 });
